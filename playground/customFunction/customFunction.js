b495cb0c8a7ab73bb362f8ed84d189e77b774071
Sameer Watts <sameer.watts00@gmail.com>
2021-05-15 18:35:45 +0530

    add parameters and arguments

--- playground/customFunction/customFunction.js
+++ playground/customFunction/customFunction.js
@@ -16,11 +16,26 @@
 // // console.log(total); error
 
 // function parameters and argument
-let bill = 100; // BAD PRACTICE
-const taxRate = 0.13; // BAD PRACTICE
+// let bill = 100; // BAD PRACTICE
+// const taxRate = 0.13; // BAD PRACTICE
+
+// // function defination
+// function calculateBill() {
+//   // specific for one value
+//   // function block
+//   // function body
+//   const total = bill * (1 + taxRate);
+//   console.log(total); // 112.99999999999999
+//   return total;
+// }
+// // function call, run, invoke
+// const myTotal1 = calculateBill(); // 112.999999
+// bill = 10;
+// const myTotal2 = calculateBill(); // 11.29999999
 
 // function defination
-function calculateBill() {
+function calculateBill(bill, taxRate = 0.13) {
+  console.log(bill, taxRate); // temp available only inside function body
   // specific for one value
   // function block
   // function body
@@ -29,6 +44,35 @@ function calculateBill() {
   return total;
 }
 // function call, run, invoke
-const myTotal1 = calculateBill(); // 112.999999
-bill = 10;
-const myTotal2 = calculateBill(); // 11.29999999
+// i can use variable and pass the variable in the function call
+const myTotal1 = calculateBill(100, 0.13); // 112.999999
+const myTotal2 = calculateBill(10, 0.13); // 11.29999999
+const myTotal3 = calculateBill(10 + 30 + 4, 0.13); // possible and will give result
+
+// const first = 'Sameer';
+function sayHiTo(first) {
+  return `hello ${first}`; // without passing parameters error
+}
+const greetings = sayHiTo('Sameer'); // hello undefined if only sayHiTo()
+console.log(greetings);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+function yell(name) {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+const yellDoc = yell(doctorize('Sam')); // HEY DR. SAM
+
+function calculateBillWithTip(bill, taxRate = 0.13, tipRate = 0.15) {
+  console.log(bill, taxRate); // temp available only inside function body
+  // specific for one value
+  // function block
+  // function body
+  const total = bill + bill * taxRate + bill * tipRate;
+  console.log(total);
+  return total;
+}
+calculateBillWithTip(100); // 128
+calculateBillWithTip(100, undefined, 0.2); // 128

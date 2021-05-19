4590f741572d96be1ebe092bdfa5b33fab674065
Sameer Watts <sameerwatts00@gmail.com>
2021-05-20 01:22:10 +0530

    Hoisting

---
+++ playground/hoisting.js
@@ -0,0 +1,20 @@
+"use strict"
+// console.log(age);
+// var age;
+// age = 22;
+
+
+// what does file do
+sayHi();
+
+
+
+// how does this file do
+function sayHi() {
+    console.log('hi');
+    console.log(add(2, 7));
+}
+
+function add (a, b) {
+    return a + b;
+}

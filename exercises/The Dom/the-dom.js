7b924446840539e0da1d43cf0ead80ce8dc3a187
Sameer Watts <sameerwatts00@gmail.com>
2021-05-22 09:05:42 +0530

    element properties and methods

--- exercises/The Dom/the-dom.js
+++ exercises/The Dom/the-dom.js
@@ -1,11 +1,30 @@
-const firstPara = document.querySelector('p');
-const innerDiv = document.querySelector('.items');
+// const firstPara = document.querySelector('p');
+// const innerDiv = document.querySelector('.items');
 
-firstPara.textContent = 'New updated paragraph';
-const paraInsideInnerDiv = innerDiv.querySelector('p.myPara');
-// console.dir(innerDiv);
-console.dir(paraInsideInnerDiv);
+// firstPara.textContent = 'New updated paragraph';
+// const paraInsideInnerDiv = innerDiv.querySelector('p.myPara');
+// // console.dir(innerDiv);
+// console.dir(paraInsideInnerDiv);
 
-const innerTestDiv2 = innerDiv.querySelector('.innerTestDiv2');
-const testPara5 = innerTestDiv2.querySelectorAll('.testPara5');
+// const innerTestDiv2 = innerDiv.querySelector('.innerTestDiv2');
+// const testPara5 = innerTestDiv2.querySelectorAll('.testPara5');
 // console.log(testPara5);
+// console.dir(firstPara);
+
+// const testHeading = document.querySelector('h2');
+// const outerDiv = document.querySelector('.outerContainer');
+// console.log(testHeading.textContent); // getter
+// testHeading.textContent = 'I am updated'; // setter
+// console.log(testHeading.textContent);
+// testHeading.innerText = 'i am updated using innet text';
+// console.log(outerDiv.innerHTML);
+// console.log(outerDiv.outerHTML);
+
+// const eatPara = document.querySelector('p');
+// eatPara.textContent = `${eatPara.textContent} golguppe`;
+// eatPara.insertAdjacentText('beforeend', '🍕');
+// eatPara.insertAdjacentText('afterbegin', '🍕');
+// eatPara.insertAdjacentText('afterend', '🍕');
+// eatPara.insertAdjacentText('beforebegin', '🍕');
+
+// getter and setter, textContent, innerText, innerHTML, outerHTML, insertAdjacentText

f574a1f2dd2a42eaeb9dfc9046fa991e82481747
Sameer Watts <sameerwatts00@gmail.com>
2021-05-26 16:51:28 +0530

    targets-bubbling-propagation-capture

--- exercises/Events/events.js
+++ exercises/Events/events.js
@@ -1,5 +1,5 @@
-const myButton = document.querySelector('.myButton');
-const checkOut = document.querySelector('.checkout');
+// const myButton = document.querySelector('.myButton');
+// const checkOut = document.querySelector('.checkout');
 
 // named function
 // myButton.addEventListener('click', function clickHandler() {
@@ -17,22 +17,48 @@ const checkOut = document.querySelector('.checkout');
 // });
 
 // function reference
-function clickHandler() {
-  console.log('clicked');
+// function clickHandler() {
+//   console.log('clicked');
+// }
+
+// myButton.addEventListener('click', clickHandler);
+// myButton.removeEventListener('click', clickHandler);
+
+// checkOut.addEventListener('click', clickHandler);
+// checkOut.removeEventListener('click', clickHandler);
+
+// function buyHandler() {
+//   console.log('buy Item');
+// }
+function buyHandler(e) {
+  //   console.log('clicked on button');
+  e.preventDefault();
+  //   e.stopPropagation();
+  console.log(e.bubbles);
+  //   console.log(e.currentTarget);
+  //   console.log(e.currentTarget === e.target);
 }
-function buyHandler() {
-  console.log('buy Item');
-}
-
-myButton.addEventListener('click', clickHandler);
-myButton.removeEventListener('click', clickHandler);
-
-checkOut.addEventListener('click', clickHandler);
-checkOut.removeEventListener('click', clickHandler);
-
 const buyButtons = document.querySelectorAll('.buy');
 
 function buyLoop(button) {
   button.addEventListener('click', buyHandler);
 }
 buyButtons.forEach(buyLoop);
+
+const image1 = document.querySelector('.firstImg');
+
+// function moveMouseHandler(e) {
+//   console.log(this);
+//   console.log(e.currentTarget);
+// }
+image1.addEventListener('click', (e) => {
+  console.log(this);
+  //   console.log(e.currentTarget);
+});
+
+// function clickWindowHandler(e) {
+//     console.log(e.target);
+//   console.log('clicked on windows');
+// }
+
+// window.addEventListener('click', clickWindowHandler, { capture: true });

96627fa76ab94a9d77e29628994eaba56f0c93d6
Sameer Watts <sameerwatts00@gmail.com>
2021-05-29 19:39:00 +0800

    coercion ternaries conditionals

---
+++ playground/LogicsAndFlowControls/coercion-ternaries-conditionals.js
@@ -0,0 +1,28 @@
+// const adminName = '';
+
+// const isLogin = !!adminName;
+
+// const admin = 'sameer';
+
+// function showAdmin() {
+//   console.log('You are admin');
+// }
+
+// admin && <Admin />;
+
+// function check1() {
+//   console.log('check 1 is running');
+//   return true;
+// }
+
+// function check2() {
+//   console.log('check 2 is running');
+//   return
+// }
+
+// // function check3() {
+// //   console.log('check 3 is running');
+// //   return true;
+// // }
+
+// const result = check1() && check2();
